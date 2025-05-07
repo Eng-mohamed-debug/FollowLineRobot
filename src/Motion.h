7e2000bbb330b1ef9b5871b2658bab148dfc2167
Eng-mohamed-debug <mohamedabdo.business2025@gmail.com>
2025-05-08 00:31:24 +0300

    adding motion header file to control the motion of  all robot

---
+++ src/Motion.h
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <Motor.h>
+
+class Motion
+{
+    static void Move(Motor motorA, Motor motorB, char *direction, int speed)
+    {
+        if (strcmp(direction, "forward") == 0)
+        {
+            motorA.Spin("forward", speed);
+            motorB.Spin("forward", speed);
+        }
+        else if (strcmp(direction, "backward") == 0)
+        {
+            motorA.Spin("backward", speed);
+            motorB.Spin("backward", speed);
+        }
+        else if (strcmp(direction, "rightward") == 0)
+        {
+            motorA.Spin("forward", speed);
+            motorB.Spin("backward", speed);
+        }
+        else if (strcmp(direction, "leftward") == 0)
+        {
+            motorB.Spin("forward", speed);
+            motorA.Spin("backward", speed);
+        }
+    }
+};
